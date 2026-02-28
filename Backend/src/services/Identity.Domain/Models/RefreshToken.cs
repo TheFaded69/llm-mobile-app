b0cce2fab3383ad2d107899fa939b7159ba6a87a
David Bidenko <thefaded69@yandex.ru>
2026-02-28 13:34:37 +0300

    Забытые файлы

---
+++ Backend/src/services/Identity.Domain/Models/RefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using BaseDomain.Models;
+
+namespace Identity.Domain.Models;
+
+public class RefreshToken : GuidEntity
+{
+    public string TokenHash { get; set; } = default!;
+    public DateTimeOffset ExpiresAt { get; set; }
+    public DateTimeOffset? RevokedAt { get; set; }
+    public string? UserAgent { get; set; }
+    public string? DeviceId { get; set; }
+
+    public string UserId { get; set; } = default!;
+    //public IdentityUser User { get; set; } = default!;
+}
---
+++ Backend/src/services/Users.Application/Handlers/AddUserHandler.cs
@@ -0,0 +1,28 @@
+﻿using Users.Contracts.V1.Requests;
+using Users.Domain.Models;
+using Users.Infrastructure.Repositories;
+
+namespace Users.Application.Handlers;
+
+public class AddUserHandler
+{
+    private readonly IUserRepository _userRepository;
+
+    public AddUserHandler(IUserRepository  userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task Handle(AddUserRequest request, CancellationToken cancellationToken)
+    {
+        var user = new User()
+        {
+            UserName = request.UserName,
+            Email = request.Email,
+            UserType = request.UserType,
+            Role = request.Role
+        };
+
+        await _userRepository.AddUserAsync(user, cancellationToken);
+    }
+}
