5ab78906d8cbce4d9075a739bed9ce0c955a31aa
David Bidenko <thefaded69@yandex.ru>
2026-05-16 18:59:07 +0300

    DI + rep

---
+++ Backend/src/Main.Infrastructure/Repositories/Tests/SessionRepository.cs
@@ -0,0 +1,34 @@
+﻿using BaseInfrastructure.Factories;
+using Main.Domain.Tests.Models;
+
+namespace Main.Infrastructure.Repositories.Tests;
+
+public class SessionRepository : ISessionRepository
+{
+    private readonly IRepositoryFactory<Session, Guid> _repositoryFactory;
+
+    public SessionRepository(IRepositoryFactory<Session, Guid> repositoryFactory)
+    {
+        _repositoryFactory = repositoryFactory;
+    }
+    
+    public IEnumerable<Session> GetSessionsByUserId(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Session>> GetSessionsByUserIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Session GetSessionById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Session> GetSessionByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
